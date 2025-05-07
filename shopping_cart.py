f21d3380b2573b44979d7cffaa82110065121705
bmoore72 <132206423+bmoore72@users.noreply.github.com>
2025-05-07 11:13:26 -0400

    cleaned data
    it worked

--- shopping_cart.py
+++ shopping_cart.py
@@ -4,10 +4,10 @@
 import pandas as pd 
 
 # Read the CSV file into a DataFrame
-df = pd.read_csv('Groceries_dataset.csv')
+df = pd.read_csv('new_Groceries_dataset.csv')
 
 # Remove duplicate rows
 df.drop_duplicates(inplace=True)
 
 # Save the DataFrame back to a CSV file
-df.to_csv('your_file.csv', index=False)
+df.to_csv('cleaned_groceries.csv', index=False)
